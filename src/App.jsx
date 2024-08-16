c4575f6a814eaf19344426dab95603ed02c045db
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-08-16 19:39:36 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import PrivateRoute from "./components/PrivateRoute";
 import RestrictedRoute from "./components/RestrictedRoute";
 import { refreshUser } from "./redux/auth/operations";
 import { selectIsRefreshing } from "./redux/auth/selectors";
-import ContactsPage from "./pages/ContactsPage";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
 
 export const App = () => {
   const dispatch = useDispatch();
